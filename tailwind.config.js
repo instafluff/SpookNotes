b160d61ab7005b845d770b9bb077161e1edca6ef
Instafluff <instaflufftv@gmail.com>
2022-06-28 21:21:32 +0200

    SpookNotes v1!

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [ "./*.{html}", "./web/**/*.{html,js}" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
