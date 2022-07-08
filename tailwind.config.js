25b5876c4e2dd0efcaecf203c0b091c8277326c4
fratorgano <francesco.torgano@outlook.it>
2022-07-08 11:01:36 +0200

    Clean tailwind files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: [ "./*.{html}", "./web/**/*.{html,js}" ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
